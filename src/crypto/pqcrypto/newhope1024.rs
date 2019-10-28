ead1ff3b6aafe07378babe56c43bc9ff02daca0b
Quentin Kniep <hello@quentinkniep.com>
2019-10-28 16:45:55 +0000

    updated for liboqs 0.2.0 and changed handshake benchmark to take number of packets as parameter

--- src/crypto/pqcrypto/newhope1024.rs
+++ src/crypto/pqcrypto/newhope1024.rs
@@ -8,14 +8,14 @@ pub const PQ_SHARED_KEY_SIZE: usize = 32;
 
 #[link(name = "oqs")]
 extern {
-    #[link_name = "OQS_KEM_newhope_1024_cca_kem_keypair"]
+    #[link_name = "OQS_KEM_newhope_1024cca_keypair"]
     pub fn pq_keypair(public_key: *mut u8,
                       secret_key: *mut u8);
-    #[link_name = "OQS_KEM_newhope_1024_cca_kem_encaps"]
+    #[link_name = "OQS_KEM_newhope_1024cca_encaps"]
     pub fn pq_encaps(ciphertext: *mut u8,
                      shared_secret: *mut u8,
                      public_key: *const u8);
-    #[link_name = "OQS_KEM_newhope_1024_cca_kem_decaps"]
+    #[link_name = "OQS_KEM_newhope_1024cca_decaps"]
     pub fn pq_decaps(shared_secret: *mut u8,
                      ciphertext: *const u8,
                      secret_key: *const u8);
